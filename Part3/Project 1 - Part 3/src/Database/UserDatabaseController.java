ad2137d0176ac24ac94d8e6ba135685a189d0ee2
PlsKillMe4654 <35269412+PlsKillMe4654@users.noreply.github.com>
2018-11-16 01:49:07 -0700

    added make payments in user database

--- Part3/Project 1 - Part 3/src/Database/UserDatabaseController.java
+++ Part3/Project 1 - Part 3/src/Database/UserDatabaseController.java
@@ -1,18 +1,45 @@
 package Database;
 
 import Domain.*;
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDatabaseController extends Controller
 {
 	private String buyerTable;
-	private String operatorTable;
+	private String operatorTable; //might not even be used
 	
 	public UserDatabaseController()
 	{
 		super("userdatabase");
 		buyerTable = "buyer";
 		operatorTable = "operator";
+	}
+	
+	public void makePayments(OrdinaryBuyer buyer, Double payment)
+	{
+		String sql = "SELECT * FROM " + buyerTable + " WHERE userID = " + buyer.getUserID() + ";";
+		ResultSet result;
+		try {
+			statement = jdbc_connection.createStatement();
+			result = statement.executeQuery(sql);
+			if(result.next()){
+				Double previous_payment = result.getDouble("outstanding_payments");
+				Double new_payment = previous_payment - payment;
+				if (new_payment < 0.0) new_payment = 0.0;
+				sql = "UPDATE " + buyerTable + 
+						" SET outstanding_payments = " + new_payment +
+						" WHERE userID = " + buyer.getUserID() + ";";
+				statement = jdbc_connection.createStatement();
+				statement.executeUpdate(sql);
+			}
+			else {
+				System.out.println("Error: Book not found");
+				
+			}
+		} catch (SQLException e) { e.printStackTrace(); }
+		
 	}
 	
 	public void register(OrdinaryBuyer ordinaryBuyer)
--- Part3/Project 1 - Part 3/src/Domain/PromotionList.java
+++ Part3/Project 1 - Part 3/src/Domain/PromotionList.java
@@ -2,11 +2,26 @@ package Domain;
 
 import java.util.ArrayList;
 
-public class PromotionList {
+public class PromotionList implements Subject {
 	private ArrayList<Observer> observers;
 	private ArrayList<Document> documents;
 	
 	public PromotionList() {
 		// TODO not exactly sure to have a default constructor
+	}
+
+	public void remove(Observer observer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void register(Observer observer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void notifyObservers() {
+		// TODO Auto-generated method stub
+		
 	}
 }	
