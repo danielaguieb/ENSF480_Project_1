14d6331d5361ccf6c01e422098cec3734c29d999
Daniel Guieb <35285976+danielaguieb@users.noreply.github.com>
2018-11-16 00:38:36 -0700

    Even more skeletons

---
+++ Part3/Project 1 - Part 3/src/Domain/Operator.java
@@ -0,0 +1,23 @@
+package Domain;
+
+import Presentation.OperatorForm;
+
+public class Operator extends User {
+
+	private OperatorForm operatorForm;
+	
+	// TODO add documents to the database
+	public void addDocuments() {
+		
+	}
+	
+	// TODO remove documents from the database
+	public void removeDocuments() {
+		
+	}
+	
+	// TODO update/edit existing document info
+	public void updateDocuments() {
+		
+	}
+}
--- Part3/Project 1 - Part 3/src/Domain/RegisteredBuyer.java
+++ Part3/Project 1 - Part 3/src/Domain/RegisteredBuyer.java
@@ -5,6 +5,17 @@ public class RegisteredBuyer extends OrdinaryBuyer {
 	private PromotionList promoList;
 	
 	public RegisteredBuyer() {
+		// TODO I'm not sure what to do about these constructors
+	}
+	
+	// TODO access the promotion list
+	public void accessPromotionList() {
 		
 	}
+	
+	// TODO unsubscribe from being a registered buyer
+	public void unsubscribe() {
+		
+	}
+	
 }
